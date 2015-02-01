ed3fa7db02bbe52aaa624e20da8a0474e81839e2
Peter Chu <peter.chu@upsight.com>
2015-01-31 23:50:22 -0500

    Remove commented code

--- Instagram.py
+++ Instagram.py
@@ -15,19 +15,10 @@ def index():
 
 @app.route('/nearby/trending')
 def trending():
-  return render_template("trending.html") 
+  return render_template("trending.html")
 
 @app.route('/nearby', methods=['POST'])
 def nearby():
-
-  #try:
-  #  media_search = api.media_search( count=10, lat=request.form['lat'], lng=request.form['long'], distance=10 )
-  #  if(media_search):
-  #    items = createJSON(media_search)
-      #print items
-  #    return jsonify(items)
-  #except UnicodeEncodeError:
-  #  pass #NOOP
 
   insta_items = grab_instagram(request)
 
@@ -35,14 +26,14 @@ def nearby():
 
   all_items = {'instagram': insta_items,
                'tweet': tweet_items}
-  
+
 
   return jsonify(createJSON(all_items))
 
 def grab_twitter(request):
   #request.form['dist'] is the distance in km.
   all_posts = twitter.search_tweets(request.form['lat'], request.form['long'], request.form['value'])
-  
+
   return all_posts
 
 """ Return a list of Item."""
@@ -56,21 +47,21 @@ def grab_instagram(request):
       for media in media_search:
         if ( hasattr(media, 'images') & ("standard_resolution" in media.images) ):
           img_urls = [media.images['standard_resolution'].url]
-        
+
         if hasattr(media.caption, 'text'):
           text = Util.remove_non_ascii(media.caption.text)
         else:
           text = ""
 
         tags = Util.extract_hashtags(text)
-          
+
         item = Item(text, img_urls, tags)
-        
+
         all_posts.append(item)
 
   except UnicodeEncodeError:
     pass #NOOP
-  
+
   return all_posts
 
 def createJSON(items):
@@ -86,11 +77,11 @@ def createJSON(items):
       i += 1
       tag_set = set(item.tags)
       all_tags = all_tags.union(tag_set)
-    
+
     json_item[key]['tags'] = list(all_tags)
     json_item[key]['num'] = i
 
-  return json_item 
+  return json_item
 
 if __name__ == '__main__':
     app.run(host="127.0.0.1", port=5000, debug=True)
