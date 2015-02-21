8417f3fa23de1f4dc5d3c648e6878f541f9dc19e
Peter Chu <cgpchu@uwaterloo.ca>
2015-02-21 15:34:58 -0500

    refactor

--- Trendy/app/src/main/java/com/example/uofthacks/trendy/MapsActivity.java
+++ Trendy/app/src/main/java/com/example/uofthacks/trendy/MapsActivity.java
@@ -9,7 +9,6 @@ import android.widget.Toast;
 import com.example.uofthacks.trendy.util.RetrieveEventsTask;
 import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.android.gms.location.LocationListener;
-import com.google.android.gms.location.LocationRequest;
 import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.maps.CameraUpdate;
 import com.google.android.gms.maps.CameraUpdateFactory;
@@ -22,7 +21,7 @@ import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
 
-public class MapsActivity extends FragmentActivity implements GoogleApiClient.ConnectionCallbacks, OnMapReadyCallback, LocationListener{
+public class MapsActivity extends FragmentActivity implements GoogleApiClient.ConnectionCallbacks, OnMapReadyCallback, LocationListener {
 
     private GoogleMap mMap; // Might be null if Google Play services APK is not available.
     private Marker marker;
@@ -40,6 +39,7 @@ public class MapsActivity extends FragmentActivity implements GoogleApiClient.Co
         mActivity = this;
         setUpMapIfNeeded();
     }
+
     @Override
     protected void onStart() {
         super.onStart();
@@ -62,18 +62,7 @@ public class MapsActivity extends FragmentActivity implements GoogleApiClient.Co
 
     /**
      * Sets up the map if it is possible to do so (i.e., the Google Play services APK is correctly
-     * installed) and the map has not already been instantiated.. This will ensure that we only ever
-     * call {@link #setUpMap()} once when {@link #mMap} is not null.
-     * <p/>
-     * If it isn't installed {@link SupportMapFragment} (and
-     * {@link com.google.android.gms.maps.MapView MapView}) will show a prompt for the user to
-     * install/update the Google Play services APK on their device.
-     * <p/>
-     * A user can return to this FragmentActivity after following the prompt and correctly
-     * installing/updating/enabling the Google Play services. Since the FragmentActivity may not
-     * have been completely destroyed during this process (it is likely that it would only be
-     * stopped or paused), {@link #onCreate(Bundle)} may not be called again so we should call this
-     * method in {@link #onResume()} to guarantee that it will be called.
+     * installed) and the map has not already been instantiated.
      */
     private void setUpMapIfNeeded() {
         // Do a null check to confirm that we have not already instantiated the map.
@@ -103,16 +92,7 @@ public class MapsActivity extends FragmentActivity implements GoogleApiClient.Co
         // updates. Gets the best and most recent location currently available, which may be null
         // in rare cases when a location is not available.
         Location mLastLocation = LocationServices.FusedLocationApi.getLastLocation(mGoogleApiClient);
-        if(mLastLocation != null) {
-            LatLng currentLatLng = new LatLng(mLastLocation.getLatitude(), mLastLocation.getLongitude());
-            CameraUpdate cameraUpdate = CameraUpdateFactory.newLatLngZoom(currentLatLng, 16);
-            // null check
-            if (mMap != null) {
-                mMap.animateCamera(cameraUpdate);
-            } else {
-                Toast.makeText(this, R.string.no_location_detected, Toast.LENGTH_LONG).show();
-            }
-        }
+        updateMapLocation();
     }
 
     @Override
@@ -151,6 +131,20 @@ public class MapsActivity extends FragmentActivity implements GoogleApiClient.Co
     @Override
     public void onLocationChanged(Location location) {
         // this callback is invoked when location updates
-        Log.d("PETER", location.toString());
+        updateMapLocation(location);
+    }
+
+    private void updateMapLocation(Location mLastLocation)
+    {
+        if(mLastLocation != null) {
+            LatLng currentLatLng = new LatLng(mLastLocation.getLatitude(), mLastLocation.getLongitude());
+            CameraUpdate cameraUpdate = CameraUpdateFactory.newLatLngZoom(currentLatLng, 16);
+            // null check
+            if (mMap != null) {
+                mMap.animateCamera(cameraUpdate);
+            } else {
+                Toast.makeText(this, R.string.no_location_detected, Toast.LENGTH_LONG).show();
+            }
+        }
     }
 }
